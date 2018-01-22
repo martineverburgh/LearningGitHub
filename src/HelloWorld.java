648ba74598efba0d3a1cba9ee4bfaa496d51ce98
Martine Verburgh <martineverburgh@gmail.com>
2018-01-22 16:43:11 +0100

    herkansing zoveel

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+
+public class HelloWorld {
+	public HelloWorld() {
+		System.out.println("Hoi Marike!");
+	}
+}
---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+
+public class Runner {
+	public static void main(String[] args) {
+		HelloWorld Hello = new HelloWorld();
+	}
+	public Runner() {
+	}
+}
