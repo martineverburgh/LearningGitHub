9478d8606e72dc3c1f8bee318f659f37a4138f7d
Martine Verburgh <martineverburgh@gmail.com>
2018-01-22 16:55:07 +0100

    werderom herkansing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@
 public class HelloWorld {
 	public HelloWorld() {
 		System.out.println("Hoi Marike!");
+		System.out.println("Wat is GitHub toch leuk he...");
+		System.out.println("Het gaat zooo soepel allemaal... zucht");
 	}
 }
